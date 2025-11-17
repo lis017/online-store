88f36985061afa0de709b8e4d3362db3831d7a43
MinHyeok Lee <minhyeok12.dev@gmail.com>
2025-11-17 13:03:45 +0900

    feat: css추가 / 고객의 상품추가기능 setter로추가  / 상품 상세페이지 틀 추가
    refactor: Thymeleaf th:each로 상품표시
    재사용용 html추가후 사용(nav.html)

--- shop/src/main/java/com/lis/shop/Item.java
+++ shop/src/main/java/com/lis/shop/Item.java
@@ -1,10 +1,18 @@
 package com.lis.shop;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity //entity로만든 클래스는 테이블하나 만드는것(클래스 이름으로)
 //이 클래스가 db랑 연동돼있다 보면됨.
+@ToString   //Lombok인데, 이걸통해 tostring하면 모든변수출력가능
+
+//이런거 쓰는 이유는, 로직에 추가 보안요소를 추가할수있어서 이다.(변수 API느낌) (이런거 추가하려면 따로 정의하긴 해야함)
+@Getter     //변수가 private일때, 이걸 다른곳에서 출력가능케하는 함수. 를 쓸수있게해줌.
+@Setter     //변수가 private일때, 이걸 다른곳에서 수정가능케하는 함수. 를 쓸수있게해줌.
 public class Item {
     //여기 변수는 컬럼 만드는거(테이블내의 옵션이름같은)
 
@@ -13,6 +21,8 @@ public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     @Column(length = 200)   //제약조건 추가 //column으로 여러 제약조건 추가 가능
-    public String title;
-    public Integer price;  //int는 JPA에서 불가
+    private String title;
+    private Integer price;  //int는 JPA에서 불가
+
+
 }
